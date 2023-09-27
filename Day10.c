4871a3c99970aa389d15c0ab7a7e704ea63fdb66
armedpixel <144881917+armedpixel@users.noreply.github.com>
2023-09-27 14:54:26 +0530

    Add files via upload

---
+++ Day10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    for(int i=0;i<t;i++){
+        int x,y;
+        scanf("%d%d",&x,&y);
+        if (1<= x,y <=100){
+            printf("%d\n",x/y);
+        }
+    }
+    return 0;
+}
